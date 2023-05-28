b25f825e3be0b81da01680df2aa7f1ea96387153
Angus Liu <angus.liu96@gmail.com>
2023-05-28 11:41:45 +0800

    feat: update icon

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,26 @@
 import * as vscode from 'vscode';
 import * as adb from 'adbkit';
+import { ThemeIcon } from 'vscode';
 
 const client = adb.createClient();
 
 export function activate(context: vscode.ExtensionContext) {
     let refreshDivicesCmd = vscode.commands.registerCommand('adb-helper.refreshDivices', function () {
-        vscode.window.showInformationMessage('refreshDivicesCmd');
         listDevices();
     });
+
+    client.trackDevices()
+        .then(function (tracker) {
+            tracker.on('add', function (device) {
+                listDevices();
+            });
+        });
 
     context.subscriptions.push(refreshDivicesCmd);
 }
 
 enum NodeType {
-    Device,
-    Dir,
-    File
+    device, dir, file
 }
 
 class DeviceNode {
@@ -23,7 +28,6 @@ class DeviceNode {
     name: string;
     path: string;
     type: NodeType;
-
     parent?: DeviceNode;
     children: DeviceNode[];
 
@@ -43,7 +47,7 @@ class DeviceNode {
         }
     }
 
-    public string(): string {
+    public toString(): string {
         return `deviceId: ${this.deviceId}, type: ${this.type}, name: ${this.name}, path: ${this.path}`;
     }
 }
@@ -52,27 +56,42 @@ class DevicesProvider implements vscode.TreeDataProvider<DeviceNode> {
 
     onDidChangeTreeData?: vscode.Event<void | DeviceNode | DeviceNode[] | null | undefined> | undefined;
 
+    private getIconPath(element: DeviceNode): ThemeIcon {
+        switch (element.type) {
+            case NodeType.device:
+                return new ThemeIcon('device-mobile');
+            case NodeType.file:
+                return ThemeIcon.File;
+            case NodeType.dir:
+                return ThemeIcon.Folder;
+        }
+    }
+
     getTreeItem(element: DeviceNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
         return {
-            label: (element.type === NodeType.Device ? '📱 ' : '') + element.name,
-            collapsibleState: element.type !== NodeType.File ? vscode.TreeItemCollapsibleState.Collapsed : void 0
+            iconPath: this.getIconPath(element),
+            label: element.name,
+            collapsibleState: element.type !== NodeType.file ? vscode.TreeItemCollapsibleState.Collapsed : void 0
         };
     }
 
-
+    private getPath(parent: DeviceNode, current: { name: string }): string {
+        return parent.type === NodeType.device ? current.name : `${parent.path}/${current.name}`;
+    }
 
     getChildren(element?: DeviceNode | undefined): vscode.ProviderResult<DeviceNode[]> {
         if (!element) {
-            return client.listDevices().map(device => new DeviceNode(device.id, NodeType.Device, device.id, '/'));
+            return client.listDevices().map(device => new DeviceNode(device.id, NodeType.device, device.id, '/'));
         }
-        vscode.window.showInformationMessage(element.string());
+
+        console.log(element);
 
         return client.readdir(element.deviceId, element.path).map(
             file => new DeviceNode(
                 element.deviceId,
-                file.isFile() ? NodeType.File : NodeType.Dir,
+                file.isFile() ? NodeType.file : NodeType.dir,
                 file.name,
-                element.type === NodeType.Device ? file.name : `${element.path}/${file.name}`,
+                this.getPath(element, file),
                 element
             )
         );
